4af5543947a88fa16b23a0f92e17f86503d7539e
afc11hn <afc11hn@hotmail.de>
2017-04-17 17:03:40 +0200

    Add first lines

--- solution_brick.py
+++ solution_brick.py
@@ -1,2 +1,10 @@
+import math
+
 # TODO: class that creates a rectangle on the canvas from the generator output
 # TODO: a parent class which holds the canvas for resizing it?
+
+class Brick:
+    def __init__(self, text):
+
+    def __len__(self):
+        return math.floor(math.log10(int(self.text)))
