bb8384887b1defbc8e33e13c2422b3173692794c
Stefan Lukas <afc11hn@hotmail.de>
2017-07-24 23:18:06 +0200

    Add .gitignore

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ from solution_brick import Brick
 
 
 def binom(n, k):
-    if 0 <= k <= n:
-        ntok = 1
-        ktok = 1
-        for t in range(1, min(k, n - k) + 1):
-            ntok *= n
-            ktok *= t
-            n -= 1
-        return ntok // ktok
-    else:
-        return 0
+	if 0 <= k <= n:
+		ntok = 1
+		ktok = 1
+		for t in range(1, min(k, n - k) + 1):
+			ntok *= n
+			ktok *= t
+			n -= 1
+		return ntok // ktok
+	else:
+		return 0
 
 
-def generator(zeilen):
-    array = []
-    for n in range(0, zeilen + 1):
-        for k in range(0, n + 1):
-            array.append(binom(n, k))
+def generator(lines):
+	array = []
+	for n in range(0, lines + 1):
+		for k in range(0, n + 1):
+			array.append(binom(n, k))
 
-        yield array
-        array = []
+		yield array
+		array = []
 
 
 size = 10
-
 
 master = Tk()
 master.wm_title("pascalsche dreiecke".title())
@@ -39,10 +38,10 @@ canvas.pack()
 ausgabe = generator(size)
 tempheight = 30
 for row in ausgabe:
-    print(row)
-    tempwidth = (cwidth / 2) - ((len(row) - 1) * 50 / 2)
-    for brick_text in row:
-        Brick(canvas, brick_text, tempwidth, tempheight)
-        tempwidth += 50
-    tempheight += 50
+	print(row)
+	tempwidth = (cwidth / 2) - ((len(row) - 1) * 50 / 2)
+	for brick_text in row:
+		Brick(canvas, brick_text, tempwidth, tempheight)
+		tempwidth += 50
+	tempheight += 50
 mainloop()
